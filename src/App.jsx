1e7b991ac72c464da81ac1d03d722de87aea5100
kabirshuvo <kabirshuvo19@gmail.com>
2023-05-06 19:13:04 +0600

    Getting user length successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 
 function App() {
-  
+  const [users, setUsers] = useState([]);
+  useEffect(()=>{
+    fetch(`http://localhost:5000/users`)
+    .then(res => res.json())
+    .then(data => setUsers(data))
+    .catch(error => console.log(error))
+  }, [])
 
   return (
     <>
       
-      <h1>Users Management Front-End</h1>
+      <h1>Users Management Front-End </h1>
+      <p>Total User = {users.length}</p>
       
     </>
   )
